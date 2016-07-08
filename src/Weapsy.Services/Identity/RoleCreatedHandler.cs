2794d1c66cc6609b98236a0ef46b57111408eb3e
Luca Briguglia <luca.briguglia@outlook.com>
2016-07-08 12:33:06 +0100

    Add RoleCreatedHandler to Identity Service

---
+++ src/Weapsy.Services/Identity/RoleCreatedHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Weapsy.Core.Domain;
+using Weapsy.Domain.Model.Roles.Events;
+
+namespace Weapsy.Domain.Model.Users.Handlers
+{
+    public class RoleCreatedHandler : IEventHandler<RoleCreated>
+    {
+        public RoleCreatedHandler()
+        {
+        }
+
+        public Task Handle(RoleCreated @event)
+        {
+            return null;
+        }
+    }
+}
