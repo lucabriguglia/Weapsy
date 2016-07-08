1960a177f99a31f4db29fbdb2585387b3ba148d6
Luca Briguglia <luca.briguglia@outlook.com>
2016-07-08 12:38:31 +0100

    Add create role validator

--- src/Weapsy.Domain/Model/Roles/Commands/CreateRole.cs
+++ src/Weapsy.Domain/Model/Roles/Commands/CreateRole.cs
@@ -1,10 +1,11 @@
-﻿using Weapsy.Core.Domain;
+﻿using System;
+using Weapsy.Core.Domain;
 
 namespace Weapsy.Domain.Model.Roles.Commands
 {
     public class CreateRole : ICommand
     {
-        public string Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
