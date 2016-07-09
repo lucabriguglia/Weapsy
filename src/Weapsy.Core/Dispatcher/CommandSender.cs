aa70d50e54cf70fc4642e51dd7e64d4b0f2348c4
Luca Briguglia <luca.briguglia@outlook.com>
2016-07-09 16:24:59 +0100

    Add logger to command sender

--- src/Weapsy.Core/Dispatcher/CommandSender.cs
+++ src/Weapsy.Core/Dispatcher/CommandSender.cs
@@ -2,6 +2,8 @@
 using Weapsy.Core.Domain;
 using Weapsy.Core.DependencyResolver;
 using AutoMapper;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
 namespace Weapsy.Core.Dispatcher
 {
@@ -10,14 +12,17 @@ namespace Weapsy.Core.Dispatcher
         private readonly IResolver _resolver;
         private readonly IEventPublisher _eventPublisher;
         private readonly IEventStore _eventStore;
+        private readonly ILogger _logger;
 
         public CommandSender(IResolver resolver, 
             IEventPublisher eventPublisher, 
-            IEventStore eventStore)
+            IEventStore eventStore,
+            ILoggerFactory loggerFactory)
         {
             _resolver = resolver;
             _eventPublisher = eventPublisher;
             _eventStore = eventStore;
+            _logger = loggerFactory.CreateLogger<CommandSender>();
         }
 
         public void Send<TCommand, TAggregate>(TCommand command, bool publishEvents = true) 
@@ -25,15 +30,32 @@ namespace Weapsy.Core.Dispatcher
             where TAggregate : IAggregateRoot
         {
             if (command == null)
+            {
+                _logger.LogError($"{nameof(command)} is null.");
                 throw new ArgumentNullException(nameof(command));
-
+            }
+                
             var commandHandler = _resolver.Resolve<ICommandHandler<TCommand>>();
 
             if (commandHandler == null)
-                throw new Exception($"No handler found for command '{command.GetType().FullName}'");
+            {
+                var errorMessage = $"No handler found for command '{command.GetType().FullName}'";
+                _logger.LogError(errorMessage);
+                throw new Exception(errorMessage);
+            }
 
-            var events = commandHandler.Handle(command);
+            ICollection<IEvent> events = new List<IEvent>();
 
+            try
+            {
+                events = commandHandler.Handle(command);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
+            
             foreach (var @event in events)
             {
                 _eventStore.SaveEvent<TAggregate>(@event);
