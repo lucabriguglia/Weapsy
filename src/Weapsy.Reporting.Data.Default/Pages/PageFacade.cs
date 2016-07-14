01defdc92da4e28645956c5db70501aa15b87d1b
Luca Briguglia <luca.briguglia@outlook.com>
2016-07-14 13:06:10 +0100

    GetPageViewModel refactoring

--- src/Weapsy.Reporting.Data.Default/Pages/PageFacade.cs
+++ src/Weapsy.Reporting.Data.Default/Pages/PageFacade.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Weapsy.Core.Caching;
 using Weapsy.Domain.Model.Languages;
 using Weapsy.Domain.Model.Modules;
 using Weapsy.Domain.Model.ModuleTypes;
 using Weapsy.Domain.Model.Pages;
 using Weapsy.Reporting.Pages;
+using Microsoft.AspNetCore.Identity;
 
 namespace Weapsy.Reporting.Data.Default.Pages
 {
@@ -20,13 +22,15 @@ namespace Weapsy.Reporting.Data.Default.Pages
         private readonly IModuleTypeRepository _moduleTypeRepository;
         private readonly ICacheManager _cacheManager;
         private readonly IMapper _mapper;
+        private readonly RoleManager<IdentityRole> _roleManager;
 
         public PageFacade(IPageRepository pageRepository, 
             ILanguageRepository languageRepository,
             IModuleRepository moduleRepository,
             IModuleTypeRepository moduleTypeRepository,
             ICacheManager cacheManager, 
-            IMapper mapper)
+            IMapper mapper,
+            RoleManager<IdentityRole> roleManager)
         {
             _pageRepository = pageRepository;
             _languageRepository = languageRepository;
@@ -34,6 +38,7 @@ namespace Weapsy.Reporting.Data.Default.Pages
             _moduleTypeRepository = moduleTypeRepository;
             _cacheManager = cacheManager;
             _mapper = mapper;
+            _roleManager = roleManager;
         }
 
         public PageViewModel GetPageViewModel(Guid siteId, Guid pageId, Guid languageId = new Guid())
@@ -54,7 +59,8 @@ namespace Weapsy.Reporting.Data.Default.Pages
                     Url = page.Url,
                     Title = page.Title,
                     MetaDescription = page.MetaDescription,
-                    MetaKeywords = page.MetaKeywords
+                    MetaKeywords = page.MetaKeywords,
+                    ViewRoles = GetViewRoles(page)
                 };
 
                 result.Page.Template = new PageTemplateModel
@@ -62,48 +68,55 @@ namespace Weapsy.Reporting.Data.Default.Pages
                     ViewName = "Default"
                 };
 
-                //var allPageModules = page.PageModules;
-                var zones = page.PageModules.Where(x => x.Status == PageModuleStatus.Active).GroupBy(x => x.Zone);
-
-                foreach (var zone in zones)
-                {
-                    var zoneModel = new ZoneModel
-                    {
-                        Name = zone.Key
-                    };
-
-                    foreach (var pageModule in zone.OrderBy(x => x.SortOrder))
-                    {
-                        var moduleModel = BuildModuleModel(pageModule);
-
-                        if (moduleModel == null)
-                            continue;
-
-                        zoneModel.Modules.Add(moduleModel);
-
-                        //var pageModuleToRemove = allPageModules.FirstOrDefault(x => x.ModuleId == moduleModel.Id);
-                        //if (pageModuleToRemove != null)
-                        //    allPageModules.Remove(pageModuleToRemove);
-                    }
-
-                    result.Zones.Add(zoneModel);
-                }
-
-                //if (allPageModules.Count > 0 && result.Zones.Count > 0)
-                //{
-                //    foreach (var pageModule in allPageModules)
-                //    {
-                //        var moduleModel = BuildModuleModel(pageModule);
-
-                //        if (moduleModel == null)
-                //            continue;
-
-                //        result.Zones.FirstOrDefault().Modules.Add(moduleModel);
-                //    }
-                //}
+                result.Zones = GetZones(page);
 
                 return result;
             });
+        }
+
+        private IList<string> GetViewRoles(Page page)
+        {
+            var roleIds = page.Permissions.Where(x => x.Type == PermissionType.View).Select(x => x.RoleId);
+
+            var result = new List<string>();
+
+            foreach (var roleId in roleIds)
+            {
+                var role = _roleManager.FindByIdAsync(roleId).Result;
+                if (role != null)
+                    result.Add(role.Name);
+            }
+
+            return result;
+        }
+
+        private ICollection<ZoneModel> GetZones(Page page)
+        {
+            var result = new List<ZoneModel>();
+
+            var zones = page.PageModules.Where(x => x.Status == PageModuleStatus.Active).GroupBy(x => x.Zone);
+
+            foreach (var zone in zones)
+            {
+                var zoneModel = new ZoneModel
+                {
+                    Name = zone.Key
+                };
+
+                foreach (var pageModule in zone.OrderBy(x => x.SortOrder))
+                {
+                    var moduleModel = BuildModuleModel(pageModule);
+
+                    if (moduleModel == null)
+                        continue;
+
+                    zoneModel.Modules.Add(moduleModel);
+                }
+
+                result.Add(zoneModel);
+            }
+
+            return result;
         }
 
         private ModuleModel BuildModuleModel(PageModule pageModule)
