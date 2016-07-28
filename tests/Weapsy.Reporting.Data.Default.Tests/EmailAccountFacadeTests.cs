66a1ce6b786868f082a8e0ee00e3241c220ac7b8
Luca Briguglia <luca.briguglia@outlook.com>
2016-07-28 13:32:08 +0100

    Add email account facade unit tests

---
+++ tests/Weapsy.Reporting.Data.Default.Tests/EmailAccountFacadeTests.cs
@@ -0,0 +1,51 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Weapsy.Core.Caching;
+using Weapsy.Domain.Model.EmailAccounts;
+using Weapsy.Reporting.Data.Default.EmailAccounts;
+using Weapsy.Reporting.EmailAccounts;
+
+namespace Weapsy.Reporting.Data.Default.Tests
+{
+    [TestFixture]
+    public class EmailAccountFacadeTests
+    {
+        private IEmailAccountFacade _sut;
+        private Guid _siteId;
+        private Guid _emailAccountId;
+
+        [SetUp]
+        public void Setup()
+        {
+            _siteId = Guid.NewGuid();
+            _emailAccountId = Guid.NewGuid();
+
+            var repositoryMock = new Mock<IEmailAccountRepository>();
+            repositoryMock.Setup(x => x.GetById(_siteId, _emailAccountId)).Returns(new EmailAccount());
+            repositoryMock.Setup(x => x.GetAll(_siteId)).Returns(new List<EmailAccount>() { new EmailAccount(), new EmailAccount() });
+
+            var cacheManagerMock = new Mock<ICacheManager>();
+
+            var mapperMock = new Mock<AutoMapper.IMapper>();
+            mapperMock.Setup(x => x.Map<EmailAccountModel>(It.IsAny<EmailAccount>())).Returns(new EmailAccountModel());
+
+            _sut = new EmailAccountFacade(repositoryMock.Object, cacheManagerMock.Object, mapperMock.Object);
+        }
+
+        [Test]
+        public void Should_return_all_models()
+        {
+            var model = _sut.GetAll(_siteId);
+            Assert.IsNotEmpty(model);
+        }
+
+        [Test]
+        public void Should_return_model()
+        {
+            var model = _sut.Get(_siteId, _emailAccountId);
+            Assert.NotNull(model);
+        }
+    }
+}
