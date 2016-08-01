85831a8aa4b112937117d065e83b85f0670d0523
Luca Briguglia <luca.briguglia@outlook.com>
2016-08-01 16:18:53 +0100

    Add knockout view model to users page

--- src/Weapsy/wwwroot/js/admin/users/index.js
+++ src/Weapsy/wwwroot/js/admin/users/index.js
@@ -1,6 +1,26 @@
 ﻿weapsy.admin.userIndex = weapsy.admin.userIndex || {};
 
-weapsy.admin.userIndex = (function ($) {
+weapsy.admin.userIndex = (function ($, ko) {
+    function User(data) {
+        this.id = ko.observable(data.id);
+        this.email = ko.observable(data.email);
+        this.editUrl = "/admin/user/edit/" + data.id;
+        this.rolesUrl = "/admin/user/roles/" + data.id;
+    }
+
+    function usersViewModel() {
+        var self = this;
+
+        self.users = ko.observableArray([]);
+
+        self.loadUsers = function () {
+            $.getJSON("/api/user/", function (data) {
+                var mappedUsers = $.map(data, function (item) { return new User(item) });
+                self.users(mappedUsers);
+            });
+        }
+    }
+
     var userIdToDelete;
 
     $('.delete-page').click(function () {
@@ -16,4 +36,12 @@ weapsy.admin.userIndex = (function ($) {
         }).fail(function () {
         });
     });
-}(jQuery));
+
+    return {
+        UsersViewModel: function () {
+            return usersViewModel();
+        }
+    }
+}(jQuery, ko));
+
+ko.applyBindings(new weapsy.admin.userIndex.UsersViewModel());
