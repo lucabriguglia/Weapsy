f022df92c72e8c6a583dfb26998a631ccd2c0b43
Luca Briguglia <luca.briguglia@outlook.com>
2016-08-02 09:21:07 +0100

    Add SqlServer data provider project for Reporting

---
+++ src/Weapsy.Reporting.Data.SqlServer/ComingSoon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Weapsy.Reporting.Data.SqlServer
+{
+    public class ComingSoon
+    {
+        public ComingSoon()
+        {
+        }
+    }
+}
