d86c07af6b4a1bf7efeb6f84fd8e783218e952af
Luca Briguglia <luca.briguglia@outlook.com>
2016-09-22 16:27:54 +0100

    Add DbContextFactory to SqlServer data provider project

---
+++ src/Weapsy.Domain.Data.SqlServer/DbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Weapsy.Domain.Data.SqlServer
+{
+    public class DbContextFactory : IDbContextFactory<WeapsyDbContext>
+    {
+        public WeapsyDbContext Create(DbContextFactoryOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
