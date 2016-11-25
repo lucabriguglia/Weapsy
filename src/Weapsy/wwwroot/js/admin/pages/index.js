9aee2c8d168d81d69987267ddca9453b704ef11f
Luca Briguglia <luca.briguglia@outlook.com>
2016-11-25 17:01:37 +0000

    Add page activation on page list

--- src/Weapsy/wwwroot/js/admin/pages/index.js
+++ src/Weapsy/wwwroot/js/admin/pages/index.js
@@ -1,6 +1,21 @@
 ﻿weapsy.admin.pageIndex = weapsy.admin.pageIndex || {};
 
 weapsy.admin.pageIndex = (function ($) {
+    $('.activate-page').click(function () {
+        var pageId = $(this).attr("data-page-id");
+        var checked = $(this).is(":checked");
+        var action = checked ? "activate" : "hide";
+        $('#savingPage').show();
+        $.ajax({
+            url: "/api/page/" + pageId + "/" + action,
+            type: "PUT"
+        }).done(function () {
+            $('#savingPage').hide();
+        }).fail(function () {
+            $('#savingPage').hide();
+        });
+    });
+
     var pageIdToDelete;
 
     $('.delete-page').click(function () {
