598b8f5d54289ba709cd86fa2fb9494faa6ca51a
Luca Briguglia <luca.briguglia@outlook.com>
2017-01-19 09:41:01 +0000

    Add SqlServer Data Provider

---
+++ src/Weapsy.Data.Providers.SqlServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("Weapsy.Data.Providers.SqlServer")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("857bb2c9-59c5-46a9-9e67-1a923122c166")]
