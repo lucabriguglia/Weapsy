4fcb8b1b08d967f33f9ae5219bdf7d59f94bd0e3
Luca Briguglia <luca.briguglia@outlook.com>
2017-02-14 16:30:12 +0000

    Update reporting project

---
+++ test/Weapsy.Reporting.Tests/Facades/Shared.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Weapsy.Data.Reporting;
+using Weapsy.Data.Repositories;
+
+namespace Weapsy.Data.Tests
+{
+    public static class Shared
+    {
+        public static IMapper CreateNewMapper()
+        {
+            var autoMapperConfig = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ReportingAutoMapperProfile());
+            });
+
+            return autoMapperConfig.CreateMapper();
+        }
+    }
+}
