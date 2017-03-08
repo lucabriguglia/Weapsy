fa190f5d25c851a7e2e07be43e0e6a63837b159e
Luca Briguglia <luca.briguglia@outlook.com>
2017-03-08 16:50:40 +0000

    Add IsAppInstalledHandler

---
+++ src/Weapsy.Data/Reporting/Apps/IsAppInstalledHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Weapsy.Domain.Apps;
+using Weapsy.Infrastructure.Queries;
+using Weapsy.Reporting.Apps.Queries;
+
+namespace Weapsy.Data.Reporting.Apps
+{
+    public class IsAppInstalledHandler : IQueryHandlerAsync<IsAppInstalled, bool>
+    {
+        private readonly IContextFactory _contextFactory;
+
+        public IsAppInstalledHandler(IContextFactory contextFactory)
+        {
+            _contextFactory = contextFactory;
+        }
+
+        public async Task<bool> RetrieveAsync(IsAppInstalled query)
+        {
+            using (var context = _contextFactory.Create())
+            {
+                return await context.Apps.Where(x => x.Name == query.Name && x.Status == AppStatus.Active).AnyAsync();
+            }
+        }
+    }
+}
