053449f265d2996e68c28daf55d866ba17f65618
Luca Briguglia <lubrigug@microsoft.com>
2019-11-15 14:21:50 +0000

    Upgrade Kledex

--- src/Weapsy.Reporting.Models/Sites/Queries/GetSiteInfo.cs
+++ src/Weapsy.Reporting.Models/Sites/Queries/GetSiteInfo.cs
@@ -2,8 +2,14 @@
 
 namespace Weapsy.Reporting.Models.Sites.Queries
 {
-    public class GetSiteInfo : IQuery<SiteInfo>
+    public class GetSiteInfo : CacheableQuery<SiteInfo>
     {
+        public GetSiteInfo(string siteName)
+        {
+            SiteName = siteName;
+            CacheKey = $"Weapsy | SiteInfo | {siteName}";
+        }
+
         public string SiteName { get; set; }
     }
 }
